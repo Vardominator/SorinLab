960ec78bfdbc06c5b5eb34637748f5bf6828f74f
Vardominator <barsegyanvarderes@gmail.com>
2016-10-07 20:54:21 -0700

    yesssss

--- Clustering/basicClusteringScript.py
+++ Clustering/basicClusteringScript.py
@@ -5,9 +5,26 @@ import matplotlib.pyplot as mpl
 
 
 def testRun():
-    testDF = pd.read_csv("C:/Users/barse/Desktop/Github/SorinLab/Clustering/freedomOfTheWorld2016.csv")
-    print(testDF)
+    
+    csvPath = "F:/GitHub/SorinLab/Clustering/freedomOfTheWorld2016.csv"
+    testDF = pd.read_csv(csvPath)
+    testDF = testDF.dropna()
 
+    testDF.set_index(testDF['Countries'])
+
+    economicSummaryIndex = testDF['EconomicFreedomSummaryIndex']
+    governmentConsumption = testDF['GovernmentConsumption']
+
+    sizeOfGovernment = testDF['SizeofGovernment']
+
+    print(economicSummaryIndex)
+    fig, ax = mpl.subplots(1)
+    fig2,ax2= mpl.subplots(1)
+    ax.scatter(economicSummaryIndex, governmentConsumption)
+    ax.set_title("Government Consumption")
+    ax2.scatter(economicSummaryIndex, sizeOfGovernment)
+    ax2.set_title("Size of Government")
+    mpl.show()
 
 if __name__ == "__main__":
     testRun()
