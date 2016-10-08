cdd35f812574281740466799d10c7c67f2845e5d
Vardominator <barsegyanvarderes@gmail.com>
2016-10-07 19:22:10 -0700

    blah

---
+++ Clustering/basicClusteringScript.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mpl
+
+
+
+def testRun():
+    testDF = pd.read_csv("C:/Users/barse/Desktop/Github/SorinLab/Clustering/freedomOfTheWorld2016.csv")
+    print(testDF)
+
+
+if __name__ == "__main__":
+    testRun()
