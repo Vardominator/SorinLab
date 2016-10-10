0ac99923dccfe6f9106bd0241bac4540bee0cfe5
Vardominator <barsegyanvarderes@gmail.com>
2016-10-09 20:15:00 -0700

    got something working

--- Clustering/basicClusteringScript.py
+++ Clustering/basicClusteringScript.py
@@ -1,30 +1,59 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mpl
-
-
+import random as rand
+import vardoVector as vect
+from kMeans import KMeans
 
 def testRun():
-    
-    csvPath = "F:/GitHub/SorinLab/Clustering/freedomOfTheWorld2016.csv"
+
+
+    csvPath = "C:/Users/barse/Desktop/Github/SorinLab/Clustering/freedomOfTheWorld2016.csv"
     testDF = pd.read_csv(csvPath)
     testDF = testDF.dropna()
 
     testDF.set_index(testDF['Countries'])
 
     economicSummaryIndex = testDF['EconomicFreedomSummaryIndex']
+
     governmentConsumption = testDF['GovernmentConsumption']
-
     sizeOfGovernment = testDF['SizeofGovernment']
+    businessRegulation = testDF['Businessregulations']
+    regulation = testDF['Regulation']
 
-    print(economicSummaryIndex)
-    fig, ax = mpl.subplots(1)
-    fig2,ax2= mpl.subplots(1)
-    ax.scatter(economicSummaryIndex, governmentConsumption)
-    ax.set_title("Government Consumption")
-    ax2.scatter(economicSummaryIndex, sizeOfGovernment)
-    ax2.set_title("Size of Government")
-    mpl.show()
+    fig, axes = mpl.subplots(nrows=2, ncols=2)
+
+    axes[0,0].scatter(governmentConsumption, economicSummaryIndex)
+    axes[0,0].set_title("Government Consumption")
+    axes[0,1].scatter(sizeOfGovernment, economicSummaryIndex)
+    axes[0,1].set_title("Size of Government")
+    axes[1,0].scatter(businessRegulation, economicSummaryIndex)
+    axes[1,0].set_title("Business Regulation")
+    axes[1,1].scatter(regulation, economicSummaryIndex)
+    axes[1,1].set_title("Regulation")
+    # mpl.show()
+
+
+
+    x = [int(i) for i in regulation]
+    y = [int(i) for i in economicSummaryIndex]
+    
+    a = np.array([x, y])
+
+    print(a)
+
+    inputs = [[-14,-5],[13,13],[20,23],[-19,-11],[-9,-16],[21,27],[-49,15],[26,13],[-46,5],[-34,-1],[11,15],[-49,0],[-22,-16],[19,28],[-12,-8],[-13,-19],[-41,8],[-11,-6],[-25,-9],[-18,-3]]
+
+
+    rand.seed(40) # so you get the same results as me
+    clusterer = KMeans(3)
+    clusterer.train(inputs)
+    print("3-means:")
+    print(clusterer.means)
+
 
 if __name__ == "__main__":
     testRun()
+
+
+
---
+++ Clustering/kMeans.py
@@ -0,0 +1,62 @@
+from vardoVector import squared_distance, vector_mean
+import random
+import itertools
+
+import matplotlib.pyplot as mpl
+
+class KMeans:
+    """performs k-means clustering"""
+
+    def __init__(self, k):
+        self.k = k          # number of clusters
+        self.means = None   # means of clusters
+        
+    def classify(self, input):
+        """return the index of the cluster closest to the input"""
+        return min(range(self.k), key=lambda i: squared_distance(input, self.means[i]))
+                   
+    def train(self, inputs):
+    
+        self.means = random.sample(inputs, self.k)
+        assignments = None
+        
+        count = 0
+        
+        while True:
+            # Find new assignments
+            new_assignments = map(self.classify, inputs)
+            
+            # If no assignments have changed, we're done.
+            print(self.means)
+            if count == 10:           
+                return
+
+            # Otherwise keep the new assignments,
+            assignments = new_assignments
+
+            for i in range(self.k):
+                i_points = [p for p, a in zip(inputs, assignments) if a == i]
+                # avoid divide-by-zero if i_points is empty
+                if i_points:                                
+                    self.means[i] = vector_mean(i_points)
+
+            count = count + 1
+
+
+if __name__ == "__main__":
+    inputs = [[-14,-5],[13,13],[20,23],[-19,-11],[-9,-16],[21,27],[-49,15],[26,13],[-46,5],[-34,-1],[11,15],[-49,0],[-22,-16],[19,28],[-12,-8],[-13,-19],[-41,8],[-11,-6],[-25,-9],[-18,-3]]
+
+    random.seed(20) # so you get the same results as me
+    clusterer = KMeans(3)
+    clusterer.train(inputs)
+    print("3-means:")
+    print(clusterer.means)
+    print(len(clusterer.means))
+
+    mpl.scatter(inputs[0], inputs[1])
+
+    #mpl.plot(inputs, 'ro')
+
+    
+
+    mpl.show()
