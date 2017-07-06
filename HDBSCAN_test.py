a36910918a57678f85c5018cf11ef028ef6189c7
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-07-06 13:10:59 -0700

    added MD hdbscan paper to resources

--- HDBSCAN_test.py
+++ HDBSCAN_test.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans, DBSCAN
 
 
-luteo = pd.read_csv('luteo_100000_sample.csv')
+luteo = pd.read_csv('luteo_10000_sample.csv')
 print(len(luteo))
 # print(luteo.columns.values)
 
@@ -14,24 +14,28 @@ print(len(luteo))
 # clusterer = hdbscan.HDBSCAN(min_cluster_size=500)
 
 # DBSCAN
-clusterer = DBSCAN(eps=0.3, min_samples=500)
+# clusterer = DBSCAN(eps=0.3, min_samples=500)
+
+# Kmeans
+clusterer = KMeans(init='k-means++', n_clusters=8, n_jobs=1)
 
 cluster_labels = clusterer.fit_predict(luteo)
 
 
-color_palette = sns.color_palette('Set2', 20)
+color_palette = sns.color_palette('hls', 10)
 
 cluster_colors = [color_palette[x] if x >= 0
                   else (0.0, 0.0, 0.0)
                   for x in clusterer.labels_]
 
-cluster_member_colors = [sns.desaturate(x, p) for x, p in
-                         zip(cluster_colors, clusterer.probabilities_)]
+# cluster_member_colors = [sns.desaturate(x, p) for x, p in
+#                          zip(cluster_colors, )]
 
 
 features = ['rmsd', 'Rg', 'NC', 'nonNC']
 
 f, axarr = plt.subplots(4, 4)
+
 
 for feature1 in features:
     for feature2 in features:
@@ -43,7 +47,7 @@ for feature1 in features:
         else:
             axarr[x, y].set_title(feature1)
 
-        axarr[x, y].scatter(luteo[feature1], luteo[feature2], s=50, linewidth=0, c=cluster_member_colors, alpha=0.25)
+        axarr[x, y].scatter(luteo[feature1], luteo[feature2], s=50, linewidth=0, c=cluster_colors, alpha=0.25)
         axarr[x, y].get_xaxis().set_visible(False)
         axarr[x, y].get_yaxis().set_visible(False)
 
