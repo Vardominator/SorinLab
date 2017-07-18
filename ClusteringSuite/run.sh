77e0057c1a8908c88690f3cc8b23c6b6f84993d8
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-07-18 14:17:11 -0700

    fixed partitioner class

--- ClusteringSuite/run.sh
+++ ClusteringSuite/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ClusteringSuite.py -d ../Data/luteo-1796-1798.txt -s 10000 -f 4,12 -N feature_scale -a hdbscan -r true -m 30,60,10
+python3 ClusteringSuite.py -d ../Data/luteo-1796-1798.txt -s 10000 -f 4,12 -N feature_scale -a hdbscan,kmeans -r true -m 50,200,50 -n 4,20,2
